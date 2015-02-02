4145a3108b94eb0413b1ed21c890896b769019ad
Joe Termine <joe@termine.com>
2015-02-02 11:20:32 -0500

    TestPromises

---
+++ TestPromiseConsole/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestPromiseConsole
+{
+    internal class Client
+    {
+        private const int sendChunkSize = 256;
+        private const int receiveChunkSize = 256;
+        private const bool verbose = true;
+        private static readonly object consoleLock = new object();
+        private static readonly TimeSpan delay = TimeSpan.FromMilliseconds(30000);
+        private static readonly UTF8Encoding encoder = new UTF8Encoding();
+        private static ClientWebSocket webSocket;
+
+        private static void Main(string[] args)
+        {
+            Thread.Sleep(1000);
+            Connect("ws://localhost.fiddler:2950/echo").Wait();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        public static async Task Connect(string uri)
+        {
+            try
+            {
+                webSocket = new ClientWebSocket();
+                await webSocket.ConnectAsync(new Uri(uri), CancellationToken.None);
+                await Task.WhenAll(Receive(), Send());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex);
+            }
+            finally
+            {
+                if (webSocket != null)
+                    webSocket.Dispose();
+                Console.WriteLine();
+
+                lock (consoleLock)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("WebSocket closed.");
+                    Console.ResetColor();
+                }
+            }
+        }
+
+        private static async Task Send()
+        {
+            //byte[] buffer = encoder.GetBytes("{\"op\":\"blocks_sub\"}"); //"{\"op\":\"unconfirmed_sub\"}");
+            byte[] buffer = encoder.GetBytes("{\"op\":\"unconfirmed_sub\"}");
+
+            for (int i = 0; i < 20; i++)
+            {
+                await
+                    webSocket.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true,
+                        CancellationToken.None);
+
+                while (webSocket.State == WebSocketState.Open)
+                {
+                    LogStatus(false, buffer, buffer.Length);
+                    await Task.Delay(delay);
+                }    
+            }
+            
+        }
+
+        private static async Task Receive()
+        {
+            var buffer = new byte[receiveChunkSize];
+            while (webSocket.State == WebSocketState.Open)
+            {
+                WebSocketReceiveResult result =
+                    await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+                }
+                else
+                {
+                    LogStatus(true, buffer, result.Count);
+                }
+            }
+        }
+
+        private static void LogStatus(bool receiving, byte[] buffer, int length)
+        {
+            lock (consoleLock)
+            {
+                Console.ForegroundColor = receiving ? ConsoleColor.Green : ConsoleColor.Gray;
+                //Console.WriteLine("{0} ", receiving ? "Received" : "Sent");
+
+                if (verbose)
+                    Console.WriteLine(encoder.GetString(buffer));
+
+                Console.ResetColor();
+            }
+        }
+    }
+}
