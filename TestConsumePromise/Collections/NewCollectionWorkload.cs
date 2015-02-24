ff5b42af7047c5daf8c423df30909b174c37c8d9
Joe Termine <joe@termine.com>
2015-02-23 23:18:10 -0500

    Added a testConfig service

---
+++ TestConsumePromise/Collections/NewCollectionWorkload.cs
@@ -0,0 +1,10 @@
+﻿using Termine.Promises.Generics;
+
+namespace TestConsumePromise.Collections
+{
+    public class NewCollectionWorkload: GenericWorkload
+    {
+        public string CollectionName { get; set; }
+        public int Visibility { get; set; }
+    }
+}
